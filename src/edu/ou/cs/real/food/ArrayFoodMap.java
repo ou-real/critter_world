920be15b9e0baaf620dad68d815e763e678ef4df
Brian M West <bmwest@ou.edu>
2015-03-30 23:16:05 -0500

    JavaDoc comments
    Added some javadoc comments

--- src/edu/ou/cs/real/food/ArrayFoodMap.java
+++ src/edu/ou/cs/real/food/ArrayFoodMap.java
@@ -6,21 +6,37 @@ import java.util.ArrayList;
 
 /**
  * Created by Brian on 3/30/2015.
+ * @author Brian M West
  */
 public class ArrayFoodMap implements FoodMappable {
     private ArrayList<Food> allFood;
 
+    /**
+     * Default constructor
+     */
     public ArrayFoodMap() {}
 
+    /**
+     * Get all instances of food on the map
+     *
+     * @return an ArrayList containing all food on the map
+     */
     public ArrayList<Food> getAll() {
         return allFood;
     }
 
+    /**
+     * Search by range for any food within a specified area
+     *
+     * @param min the upper left coordinates of the range to search
+     * @param max the bottom right coordinates of the range to search
+     * @return an ArrayList containing the food within the range
+     */
     public ArrayList<Food> rangeSearch(Vec2d min, Vec2d max) {
         ArrayList<Food> food = new ArrayList<Food>();
 
         for (Food f : allFood) {
-            Vec2d p = f.getPosition();
+            Vec2d p = f.getLocation();
             if (p.x >= min.x && p.x <= max.x && p.y >= min.y && p.y <= max.y) {
                 food.add(f);
             }
@@ -29,6 +45,11 @@ public class ArrayFoodMap implements FoodMappable {
         return food;
     }
 
+    /**
+     * Add food to the map
+     *
+     * @param food the food instance to add (which already should contain coordinates)
+     */
     public void addFood(Food food) {
         allFood.add(food);
     }
--- src/edu/ou/cs/real/food/Food.java
+++ src/edu/ou/cs/real/food/Food.java
@@ -14,6 +14,11 @@ public class Food {
 
     ArrayList<Critterable> claims;
 
+    /**
+     * Create food with default value at location
+     *
+     * @param location location of where to create the food
+     */
     public Food(Vec2d location) {
         this.location = location;
 
@@ -22,6 +27,12 @@ public class Food {
         claims = new ArrayList<Critterable>();
     }
 
+    /**
+     * Create food with a given value at location
+     *
+     * @param location location of where to create the food
+     * @param value caloric value of the food
+     */
     public Food(Vec2d location, float value) {
         this.location = location;
 
@@ -30,23 +41,46 @@ public class Food {
         claims = new ArrayList<Critterable>();
     }
 
+    /**
+     * Remove all claims on the piece of food
+     */
     public void emptyClaims(){
         claims.clear();
     }
 
+    /**
+     * Get the caloric value of this food
+     *
+     * @return the double value of the food's worth
+     */
     public double getValue() {
         return value;
     }
 
+    /**
+     * Get all the Critterable instances that have laid claim to this food
+     *
+     * @return An ArrayList of claiming individuals
+     */
     public ArrayList<Critterable> getClaims(){
         return claims;
     }
 
+    /**
+     * Update the list of claims to include the given critter
+     *
+     * @param critter the critter laying claim
+     */
     public void addClaim(Critterable critter) {
         claims.add(critter);
     }
 
-    public Vec2d getPosition(){
+    /**
+     * Get the location of this food
+     *
+     * @return a Vec2d representing the x,y coordinates of this food
+     */
+    public Vec2d getLocation(){
         return location;
     }
 }
